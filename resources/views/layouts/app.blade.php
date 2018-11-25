a583a4784cb5735307ecdb563905f124f06a59aa
ikeogu <ikeogu322@gmail.com>
2018-11-25 21:25:01 +0100

    push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,16 +47,16 @@
 		<link rel="stylesheet" href="{{ asset('css/bootstrap.css')}}">
 		<link rel="stylesheet" href="{{ asset('css/main.css')}}">
    <!-- Animation library for notifications   -->
-        <script src="{{ asset('js/vendor/jquery-2.2.4.min.js"></script>
+        <script src="js/vendor/jquery-2.2.4.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-		<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-		<script src="{{ asset('js/jquery.ajaxchimp.min.js') }}"></script>
-		<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
-		<script src="{{ asset('js/jquery.nice-select.min.js') }}"></script>
-		<script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
-		<script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
-		<script src="{{ asset('js/waypoints.min.js') }}"></script>
-		<script src="{{ asset('js/main.js') }}"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.ajaxchimp.min.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/jquery.nice-select.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="js/jquery.counterup.min.js"></script>
+		<script src="js/waypoints.min.js"></script>
+		<script src="js/main.js"></script>
 
 
 </head>
