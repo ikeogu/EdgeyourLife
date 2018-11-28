36a2a78a82b050492804bb672f097a1c6c7442d7
ikeogu <ikeogu322@gmail.com>
2018-11-28 12:39:45 +0100

    dkfljg

--- app/Role.php
+++ app/Role.php
@@ -11,7 +11,7 @@ class Role extends Model
         'name','description',
     ];
     
-    public function users(){
+    public function user(){
         return $this->hasMany('App\User');      
     }
 
